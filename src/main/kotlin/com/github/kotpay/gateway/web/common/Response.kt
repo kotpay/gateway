0f9a4a6fc2cdb9dac90fac108b650065a82359b2
leuvaarden <probablyviolin@gmail.com>
2022-04-28 21:22:30 +0300

    Fix

--- src/main/kotlin/com/github/kotpay/gateway/web/common/Response.kt
+++ src/main/kotlin/com/github/kotpay/gateway/web/common/Response.kt
@@ -1,9 +1,11 @@
 package com.github.kotpay.gateway.web.common
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.v3.oas.annotations.media.Schema
 
 
 interface Response<T> {
+    @JsonProperty(index = 0)
     @Schema(description = "Flag denoting whether operation was successful", required = true)
     fun getSuccess(): Boolean
 
