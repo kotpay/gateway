e628dc017da4ac7060f7d61eb64dcf157c7e50d3
leuvaarden <probablyviolin@gmail.com>
2022-05-18 09:34:38 +0300

    Fix

--- src/main/kotlin/com/github/kotpay/gateway/web/controller/order/OrderController.kt
+++ src/main/kotlin/com/github/kotpay/gateway/web/controller/order/OrderController.kt
@@ -10,12 +10,14 @@ import com.github.kotpay.gateway.web.controller.order.dto.OrderStatusResponse
 import io.swagger.v3.oas.annotations.Operation
 import mu.KotlinLogging
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin(origins = ["*"])
 @RequestMapping("/order", consumes = [APPLICATION_JSON_VALUE], produces = [APPLICATION_JSON_VALUE])
 class OrderController(
     private val orderService: OrderService,
--- src/main/kotlin/com/github/kotpay/gateway/web/controller/payment/PaymentController.kt
+++ src/main/kotlin/com/github/kotpay/gateway/web/controller/payment/PaymentController.kt
@@ -9,12 +9,14 @@ import com.github.kotpay.gateway.web.controller.payment.dto.PaymentCardResponse
 import io.swagger.v3.oas.annotations.Operation
 import mu.KotlinLogging
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin(origins = ["*"])
 @RequestMapping("/payment", consumes = [APPLICATION_JSON_VALUE], produces = [APPLICATION_JSON_VALUE])
 class PaymentController(
     private val orderService: OrderService,
